9dbaeb74ae19d2f0e9c6901222e5a27c637c8bf3
Ekaterina El <hackerrussia@yandex.ru>
2023-04-29 15:48:41 +0500

    Added function menu on the accident screen

--- app/src/main/java/com/elka/servicedesk/view/ui/accident/AccidentFragment.kt
+++ app/src/main/java/com/elka/servicedesk/view/ui/accident/AccidentFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
@@ -11,6 +12,8 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.elka.servicedesk.R
 import com.elka.servicedesk.databinding.AccidentFragmentBinding
+import com.elka.servicedesk.other.AccidentStatus
+import com.elka.servicedesk.other.Role
 import com.elka.servicedesk.other.Work
 import com.elka.servicedesk.service.model.Accident
 import com.elka.servicedesk.service.model.Log
@@ -21,114 +24,217 @@ import com.elka.servicedesk.view.ui.UserBaseFragment
 import com.elka.servicedesk.viewModel.AccidentsViewModel
 
 class AccidentFragment : UserBaseFragment() {
-  private lateinit var binding: AccidentFragmentBinding
-  private val accidentsViewModel by activityViewModels<AccidentsViewModel>()
+	private lateinit var binding: AccidentFragmentBinding
+	private val accidentsViewModel by activityViewModels<AccidentsViewModel>()
 
-  private val logsAdapter by lazy { LogsAdapter() }
+	private val logsAdapter by lazy { LogsAdapter() }
 
-  private val accidentLogsObserver = Observer<List<Log>> {
-    logsAdapter.setItems(it)
-  }
+	private val accidentLogsObserver = Observer<List<Log>> {
+		logsAdapter.setItems(it)
+	}
 
 
-  private val imagesAdapter: ImagesAdapter by lazy {
-    ImagesAdapter(null, null)
-  }
+	private val imagesAdapter: ImagesAdapter by lazy {
+		ImagesAdapter(null, null)
+	}
 
-  private val accidentObserver = Observer<Accident?>{accident ->
-    if (accident == null) return@Observer
+	private val accidentObserver = Observer<Accident?> { accident ->
+		if (accident == null) return@Observer
 
-    val items = accident.photosURL.map { ImageItem(type = ImagesAdapter.TYPE_ITEM, it) }.toMutableList()
-    imagesAdapter.setItems(items)
-  }
+    updateMenuStatus()
 
-  private val works = listOf(
-    Work.LOAD_ACCIDENT
-  )
+		val items =
+			accident.photosURL.map { ImageItem(type = ImagesAdapter.TYPE_ITEM, it) }.toMutableList()
+		imagesAdapter.setItems(items)
+	}
 
-  override val workObserver = Observer<List<Work>> {
-    val w1 = userViewModel.work.value!!.toMutableList()
-    val w2 = accidentsViewModel.work.value!!
-    w1.addAll(w2)
-    val hasLoads = getHasLoads(w1, works)
+	private val works = listOf(
+		Work.LOAD_ACCIDENT
+	)
 
-    binding.swiper.isRefreshing = hasLoads
-  }
+	private val hasLoads: Boolean
+		get() {
+			val w1 = userViewModel.work.value!!.toMutableList()
+			val w2 = accidentsViewModel.work.value!!
+			w1.addAll(w2)
+			return getHasLoads(w1, works)
+		}
 
-  override fun onCreateView(
-    inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-  ): View {
-    binding = AccidentFragmentBinding.inflate(layoutInflater, container, false)
-    binding.apply {
-      lifecycleOwner = viewLifecycleOwner
-      master = this@AccidentFragment
-      viewModel = this@AccidentFragment.accidentsViewModel
-      imagesAdapter = this@AccidentFragment.imagesAdapter
-      logsAdapter = this@AccidentFragment.logsAdapter
-    }
+	override val workObserver = Observer<List<Work>> {
+		binding.swiper.isRefreshing = hasLoads
+	}
 
-    return binding.root
-  }
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+	): View {
+		binding = AccidentFragmentBinding.inflate(layoutInflater, container, false)
+		binding.apply {
+			lifecycleOwner = viewLifecycleOwner
+			master = this@AccidentFragment
+			viewModel = this@AccidentFragment.accidentsViewModel
+			imagesAdapter = this@AccidentFragment.imagesAdapter
+			logsAdapter = this@AccidentFragment.logsAdapter
+		}
 
-  fun goBack() {
-    accidentsViewModel.clearCurrentAccident()
-    navController.popBackStack()
-  }
+		return binding.root
+	}
 
-  private val onBackPressedCallback by lazy {
-    object : OnBackPressedCallback(true) {
-      override fun handleOnBackPressed() {
-        goBack()
+	fun goBack() {
+		accidentsViewModel.clearCurrentAccident()
+		navController.popBackStack()
+	}
+
+	private val onBackPressedCallback by lazy {
+		object : OnBackPressedCallback(true) {
+			override fun handleOnBackPressed() {
+				goBack()
+			}
+		}
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+
+		// get args
+		val args = AccidentFragmentArgs.fromBundle(requireArguments())
+		accidentsViewModel.loadCurrentOpenAccident(args.accidentId)
+
+		// add decorator to list
+		val decorator = DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
+		binding.logsList.addItemDecoration(decorator)
+
+		// add on back press listener
+		activity.onBackPressedDispatcher.addCallback(onBackPressedCallback)
+
+		// work with swipe refresher
+		val refresherColor = requireContext().getColor(R.color.accent)
+		val swipeRefreshListener = SwipeRefreshLayout.OnRefreshListener { reloadAccident() }
+
+		binding.swiper.setColorSchemeColors(refresherColor)
+		binding.swiper.setOnRefreshListener(swipeRefreshListener)
+	}
+
+	private fun reloadAccident() {
+		accidentsViewModel.reloadCurrentAccident()
+	}
+
+	override fun onResume() {
+		super.onResume()
+
+		userViewModel.error.observe(this, errorObserver)
+		userViewModel.work.observe(this, workObserver)
+		accidentsViewModel.error.observe(this, errorObserver)
+		accidentsViewModel.work.observe(this, workObserver)
+		accidentsViewModel.currentAccident.observe(this, accidentObserver)
+		accidentsViewModel.currentAccidentLogs.observe(this, accidentLogsObserver)
+	}
+
+	override fun onStop() {
+		super.onStop()
+
+		userViewModel.error.removeObserver(errorObserver)
+		userViewModel.work.removeObserver(workObserver)
+		accidentsViewModel.error.removeObserver(errorObserver)
+		accidentsViewModel.work.removeObserver(workObserver)
+		accidentsViewModel.currentAccident.removeObserver(accidentObserver)
+		accidentsViewModel.currentAccidentLogs.removeObserver(accidentLogsObserver)
+	}
+
+	private val menu by lazy {
+		val popupMenu = PopupMenu(context, binding.menu)
+		popupMenu.setOnMenuItemClickListener {
+			when (it.itemId) {
+				ACCEPT_ACCIDENT_TO_WORK -> Unit
+				CLOSE_ACCIDENT -> Unit
+				ACCEPT_CLOSE_ACCIDENT -> Unit
+				DENY_CLOSE_ACCIDENT -> Unit
+				EXCALATION -> Unit
+				else -> Unit
+			}
+			return@setOnMenuItemClickListener true
+		}
+
+		return@lazy popupMenu
+	}
+
+  private fun updateMenuStatus() {
+    val role = userViewModel.profile.value!!.role
+    val accidentStatus = accidentsViewModel.currentAccident.value!!.status
+
+    val viewStatus = when(role) {
+      Role.USER -> {
+        when(accidentStatus) {
+          AccidentStatus.READY,
+          AccidentStatus.WAITING -> View.VISIBLE
+          else -> View.INVISIBLE
+        }
       }
+      Role.ENGINEER -> {
+        when(accidentStatus) {
+          AccidentStatus.ACTIVE,
+          AccidentStatus.IN_WORK -> View.VISIBLE
+          else -> View.INVISIBLE
+        }
+      }
+      else -> View.INVISIBLE
     }
+
+    binding.menu.visibility = viewStatus
   }
 
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
+	private fun inflateMenu() {
+		val menu = menu.menu
+		menu.clear()
 
-    // get args
-    val args = AccidentFragmentArgs.fromBundle(requireArguments())
-    accidentsViewModel.loadCurrentOpenAccident(args.accidentId)
+		val role = userViewModel.profile.value!!.role
+		val accidentStatus = accidentsViewModel.currentAccident.value!!.status
 
-    // add decorator to list
-    val decorator = DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
-    binding.logsList.addItemDecoration(decorator)
+    when (role) {
+      Role.USER -> {
+        when (accidentStatus) {
+          AccidentStatus.READY -> {
+            menu.add(0, ACCEPT_CLOSE_ACCIDENT, 0, R.string.accept_close_accident)
+            menu.add(0, DENY_CLOSE_ACCIDENT, 0, R.string.deny_close_accident)
+          }
+          AccidentStatus.WAITING -> {
+            menu.add(0, ADD_INFORMATION, 0, R.string.add_information)
+          }
+          else -> Unit
+        }
+      }
+      Role.ENGINEER -> {
+        when (accidentStatus) {
+          AccidentStatus.ACTIVE -> {
+            menu.add(0, ACCEPT_ACCIDENT_TO_WORK, 0, R.string.accept_accident_to_work)
+          }
+          AccidentStatus.IN_WORK -> {
+            menu.add(0, CLOSE_ACCIDENT, 0, R.string.close_accident)
+            menu.add(0, EXCALATION, 0, R.string.excalation)
+          }
+          else -> Unit
+        }
+      }
+      else -> Unit
+    }
 
-    // add on back press listener
-    activity.onBackPressedDispatcher.addCallback(onBackPressedCallback)
+	}
 
-    // work with swipe refresher
-    val refresherColor = requireContext().getColor(R.color.accent)
-    val swipeRefreshListener = SwipeRefreshLayout.OnRefreshListener { reloadAccident() }
+	fun openMenu() {
+		if (hasLoads) {
+			showLoadingErrorMessage()
+			return
+		}
 
-    binding.swiper.setColorSchemeColors(refresherColor)
-    binding.swiper.setOnRefreshListener(swipeRefreshListener)
-  }
+		inflateMenu()
+		menu.show()
+	}
 
-  private fun reloadAccident() {
-    accidentsViewModel.reloadCurrentAccident()
-  }
-
-  override fun onResume() {
-    super.onResume()
-
-    userViewModel.error.observe(this, errorObserver)
-    userViewModel.work.observe(this, workObserver)
-    accidentsViewModel.error.observe(this, errorObserver)
-    accidentsViewModel.work.observe(this, workObserver)
-    accidentsViewModel.currentAccident.observe(this, accidentObserver)
-    accidentsViewModel.currentAccidentLogs.observe(this, accidentLogsObserver)
-  }
-
-  override fun onStop() {
-    super.onStop()
-
-    userViewModel.error.removeObserver(errorObserver)
-    userViewModel.work.removeObserver(workObserver)
-    accidentsViewModel.error.removeObserver(errorObserver)
-    accidentsViewModel.work.removeObserver(workObserver)
-    accidentsViewModel.currentAccident.removeObserver(accidentObserver)
-    accidentsViewModel.currentAccidentLogs.removeObserver(accidentLogsObserver)
-
-  }
+	companion object {
+		const val ACCEPT_ACCIDENT_TO_WORK = 1
+		const val CLOSE_ACCIDENT = 2
+		const val ACCEPT_CLOSE_ACCIDENT = 3
+		const val DENY_CLOSE_ACCIDENT = 4
+		const val EXCALATION = 5
+		const val ADD_INFORMATION = 6
+	}
 }
