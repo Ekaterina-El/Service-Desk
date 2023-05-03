5ced092d39a716b3e55540519e884f8486158c8b
EkaterinaEl <hackerrussia@yandex.ru>
2023-05-03 21:48:18 +0500

    Show reason and sender of excalation

--- app/src/main/java/com/elka/servicedesk/service/model/Accident.kt
+++ app/src/main/java/com/elka/servicedesk/service/model/Accident.kt
@@ -6,97 +6,104 @@ import com.elka.servicedesk.view.list.accidents.AccidentsAdapter
 import java.util.*
 
 data class Accident(
-  var type: AccidentType = AccidentType.INCIDENT,
-  var id: String = "",
+	var type: AccidentType = AccidentType.INCIDENT,
+	var id: String = "",
 
-  var subject: String = "",
-  var message: String = "",
+	var subject: String = "",
+	var message: String = "",
 
-  var urgency: UrgencyCategory = UrgencyCategory.LOW,
-  var category: AccidentCategory = AccidentCategory.NETWORKING_TECHNOLOGIES,
+	var urgency: UrgencyCategory = UrgencyCategory.LOW,
+	var category: AccidentCategory = AccidentCategory.NETWORKING_TECHNOLOGIES,
 
-  var photosURL: List<String> = listOf(),
+	var photosURL: List<String> = listOf(),
 
-  var divisionId: String = "",
-  var divisionLocal: Division? = null,
+	var divisionId: String = "",
+	var divisionLocal: Division? = null,
 
-  var userId: String = "",
-  var userLocal: User? = null,
+	var userId: String = "",
+	var userLocal: User? = null,
 
-  var engineerId: String? = null,
-  var engineerLocal: User? = null,
+	var engineerId: String? = null,
+	var engineerLocal: User? = null,
 
-  var moreInfo: List<AccidentMoreInfo> = listOf(),
+	var moreInfo: List<AccidentMoreInfo> = listOf(),
 
-  var reasonOfExcalation: String = "",
-  var senderOfExcalation: User? = null,
+	var reasonOfExcalation: String = "",
+	var senderOfExcalation: User? = null,
 
-  var createdDate: Date = Constants.getCurrentDate(),
-  var status: AccidentStatus = AccidentStatus.ACTIVE,
+	var createdDate: Date = Constants.getCurrentDate(),
+	var status: AccidentStatus = AccidentStatus.ACTIVE,
 
-): java.io.Serializable {
-  var createdDateS: String
-    get() = createdDate.toLogFormat()
-    set(v) {}
+	) : java.io.Serializable {
 
-  var info: String
-    get() = "${urgency.text} срочность / ${divisionLocal?.name} / ${userLocal?.fullName} / $createdDateS"
-    set(v) {}
+	var senderOfExcalationS: String
+		get() = "${senderOfExcalation?.fullName} (${senderOfExcalation?.role?.text})"
+		set(v) {}
+
+	var createdDateS: String
+		get() = createdDate.toLogFormat()
+		set(v) {}
+
+	var info: String
+		get() = "${urgency.text} срочность / ${divisionLocal?.name} / ${userLocal?.fullName} / $createdDateS"
+		set(v) {}
 }
 
 fun List<Accident>.filterBy(s: String) = this.filter {
-  it.subject.contains(s, true)
-      || it.message.contains(s, true)
-      || it.urgency.text.contains(s, true)
-      || it.category.text.contains(s, true)
-      || it.divisionLocal?.name?.contains(s, true) == true
-      || it.userLocal?.fullName?.contains(s, true) == true
-      || it.createdDateS.contains(s, true)
-      || it.status.text.contains(s, true)
+	it.subject.contains(s, true) || it.message.contains(s, true) || it.urgency.text.contains(
+		s,
+		true
+	) || it.category.text.contains(s, true) || it.divisionLocal?.name?.contains(
+		s,
+		true
+	) == true || it.userLocal?.fullName?.contains(s, true) == true || it.createdDateS.contains(
+		s,
+		true
+	) || it.status.text.contains(s, true)
 }
 
 fun List<Accident>.splitAndSort(): MutableList<Accident> {
-  val items = mutableListOf<Accident>()
-  val incidents =
-    this.filter { it.type == AccidentType.INCIDENT }.sortedByDescending { it.createdDate }
-  val requests =
-    this.filter { it.type == AccidentType.REQUEST }.sortedByDescending { it.createdDate }
+	val items = mutableListOf<Accident>()
+	val incidents =
+		this.filter { it.type == AccidentType.INCIDENT }.sortedByDescending { it.createdDate }
+	val requests =
+		this.filter { it.type == AccidentType.REQUEST }.sortedByDescending { it.createdDate }
 
-  items.addAll(incidents)
-  items.addAll(requests)
+	items.addAll(incidents)
+	items.addAll(requests)
 
-  return items
+	return items
 }
 
 
 fun List<Accident>.toAccidentItems() = this.map { AccidentItem(AccidentsAdapter.TYPE_ITEM, it) }
 
 fun List<Accident>.allToAccidentItems(): List<AccidentItem> {
-  val items = mutableListOf<AccidentItem>()
-  val incidents =
-    this.filter { it.type == AccidentType.INCIDENT }.sortedByDescending { it.createdDate }
-  val requests =
-    this.filter { it.type == AccidentType.REQUEST }.sortedByDescending { it.createdDate }
+	val items = mutableListOf<AccidentItem>()
+	val incidents =
+		this.filter { it.type == AccidentType.INCIDENT }.sortedByDescending { it.createdDate }
+	val requests =
+		this.filter { it.type == AccidentType.REQUEST }.sortedByDescending { it.createdDate }
 
-  if (incidents.isNotEmpty()) {
-    items.add(
-      AccidentItem(
-        type = AccidentsAdapter.TYPE_HEADER, value = AccidentType.INCIDENT.text
-      )
-    )
+	if (incidents.isNotEmpty()) {
+		items.add(
+			AccidentItem(
+				type = AccidentsAdapter.TYPE_HEADER, value = AccidentType.INCIDENT.text
+			)
+		)
 
-    items.addAll(incidents.toAccidentItems())
-  }
+		items.addAll(incidents.toAccidentItems())
+	}
 
-  if (requests.isNotEmpty()) {
-    items.add(
-      AccidentItem(
-        type = AccidentsAdapter.TYPE_HEADER, value = AccidentType.REQUEST.text
-      )
-    )
+	if (requests.isNotEmpty()) {
+		items.add(
+			AccidentItem(
+				type = AccidentsAdapter.TYPE_HEADER, value = AccidentType.REQUEST.text
+			)
+		)
 
-    items.addAll(requests.toAccidentItems())
-  }
+		items.addAll(requests.toAccidentItems())
+	}
 
-  return items
+	return items
 }
