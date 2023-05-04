f3657208faf6d0f497a367d9f75d37a94183143d
EkaterinaEl <hackerrussia@yandex.ru>
2023-05-04 12:58:57 +0500

    No show deadline of accident with type REQUEST

--- app/src/main/java/com/elka/servicedesk/view/databinding/textBinding.kt
+++ app/src/main/java/com/elka/servicedesk/view/databinding/textBinding.kt
@@ -1,19 +1,19 @@
 package com.elka.servicedesk.view.databinding
 
+import android.view.View
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.elka.servicedesk.R
 import com.elka.servicedesk.other.AccidentStatus
+import com.elka.servicedesk.other.AccidentType
 import com.elka.servicedesk.service.model.Accident
 
 @BindingAdapter("app:statusColor")
 fun setStatusColor(textView: TextView, status: AccidentStatus?) {
-	val colorRes = when(status) {
+	val colorRes = when (status) {
 		AccidentStatus.IN_WORK -> R.color.in_work
 
-		AccidentStatus.CLOSED,
-		AccidentStatus.WAITING,
-		AccidentStatus.ESCALATION -> R.color.attention
+		AccidentStatus.CLOSED, AccidentStatus.WAITING, AccidentStatus.ESCALATION -> R.color.attention
 
 		AccidentStatus.READY -> R.color.ready
 
@@ -24,12 +24,14 @@ fun setStatusColor(textView: TextView, status: AccidentStatus?) {
 
 @BindingAdapter("app:leftTime")
 fun setLeftTime(textView: TextView, accident: Accident?) {
-	if (accident == null) {
+	if (accident == null || accident.type == AccidentType.REQUEST) {
 		textView.text = ""
+		textView.visibility = View.GONE
 		return
 	}
 
 	val textColorRes = if (accident.executionTime == null) R.color.attention else R.color.on_primary
 	textView.setTextColor(textView.context.getColor(textColorRes))
 	textView.text = accident.executionTimeS
+	textView.visibility = View.VISIBLE
 }
